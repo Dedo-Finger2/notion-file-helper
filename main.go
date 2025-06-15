271a2334620cb1105089664ca24b69dcf285a432
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-06-15 20:14:17 -0300

    feat: explanation (portuguese)

--- main.go
+++ main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+/*
+	Este projeto consiste em um
+	helper para o Notion
+	onde ele:
+
+	Com áudios, ele divide em vários outros
+	arquivos de áudio com um tamanho
+	máximo de 4,9MB por arquivo.
+	Depois, coloca tudo dentro de um zip
+	e joga isso numa pasta do usuário.
+
+	Com imagens, ele reduzirá a qualidade
+	da imagem até onde der, tentando deixar
+	ela com no máximo 4,9MB de tamanho.
+
+	Com vídeos, ele fará o mesmo processo
+	feito com os áudios.
+*/
+
 func main() {
 	fmt.Println("Hello, World!")
 }
