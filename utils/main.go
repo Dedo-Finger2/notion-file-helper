ce3f6302aaa5699f474097c67c2470c092915e31
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-06-15 21:13:43 -0300

    feat: adds CreateZipFileWithFiles func

--- utils/main.go
+++ utils/main.go
@@ -1,6 +1,10 @@
 package utils
 
-import "os"
+import (
+	"archive/zip"
+	"io"
+	"os"
+)
 
 // Returns size in bytes, megabytes and a possible error.
 func GetFileSizeFromPath(path string) (int64, float64, error) {
@@ -14,4 +18,31 @@ func GetFileSizeFromPath(path string) (int64, float64, error) {
 		return 0, 0, err
 	}
 	return fInfo.Size(), float64(fInfo.Size()) / float64(1000000), nil
+}
+
+func CreateZipFileWithFiles(name string, files []string) error {
+	zipFile, err := os.Create(name + ".zip")
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+	for _, file := range files {
+		srcFile, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer srcFile.Close()
+		zipEntry, err := zipWriter.Create(file)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(zipEntry, srcFile)
+		if err != nil {
+			return err
+		}
+		os.Remove(file)
+	}
+	return nil
 }
